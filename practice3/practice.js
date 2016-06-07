7af0dcb457e245c88c7ea3e2edfa30ecd95aa92b
Thwipshot <ethumpert@gmail.com>
2016-06-07 13:56:22 -0500

    solved the 3rd practice problem

--- practice3/practice.js
+++ practice3/practice.js
@@ -5,7 +5,17 @@
 
   //TODO: Create a View called "SimpleModelView" and implement what is necessary.
 
-  //var view = new SimpleModelView({ el: $('#simple-model-view'), model: fred });
-  //view.render();
+  var SimpleModelView = Backbone.View.extend({
+
+    render: function() {
+      var template = _.template("<p>A view for <%= name %> (<%= age %>)!</p>");
+      this.$el.html(template({name: fred.get("name"), age: fred.get("age")}));
+      return this;
+    }
+  })
+
+
+  var view = new SimpleModelView({ el: $('#simple-model-view'), model: fred });
+  view.render();
 
 })();
